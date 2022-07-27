670bbbb92d6dbb39c06e3320b2231ddee9c20143
Imon <contact@imonweb.co.uk>
2022-07-27 22:16:07 +0100

    part4

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+import './style.css'
+
 const navToggle = document.querySelector('.mobile-nav-toggle');
 const primaryNav = document.querySelector('.primary-navigation');
 
 navToggle.addEventListener('click', () => {
-  primaryNav.classList.toggle('opened');
+  // console.log('clicked');
+  // primaryNav.classList.toggle('opened');
+  primaryNav.hasAttribute('data-visible') 
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute('data-visible');
 })
